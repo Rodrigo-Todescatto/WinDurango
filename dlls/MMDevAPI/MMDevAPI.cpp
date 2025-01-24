91557a0390e5101d3bc4b3ab2de19be1cfc89a06
Darien Johnson <84008186+CADIndie@users.noreply.github.com>
2025-01-24 13:21:35 -0500

    Stub PackageTransferManager

--- dlls/MMDevAPI/MMDevAPI.cpp
+++ dlls/MMDevAPI/MMDevAPI.cpp
@@ -1,42 +1,43 @@
 #include "pch.h"
 #include "framework.h"
+#include <stdio.h>
 
 void ActivateAudioInterfaceAsync_X()
 {
-
+	printf("!!! MMDevApi || ActivateAudioInterfaceAsync Not Implemented !!!\n");
 }
 
 void DisableBitstreamOut_X()
 {
-
+	printf("!!! MMDevApi || DisableBitstreamOut Not Implemented !!!\n");
 }
 
 void DllCanUnloadNow_X()
 {
-
+	printf("!!! MMDevApi || DllCanUnloadNow Not Implemented !!!\n");
 }
 
 void DllGetClassObject_X()
 {
-
+	printf("!!! MMDevApi || DllGetClassObject Not Implemented !!!\n");
 }
 
 void EnableSpatialAudio_X()
 {
-
+	printf("!!! MMDevApi || EnableSpatialAudio Not Implemented !!!\n");
 }
 
 void RefreshWasapiDeviceList_X()
 {
-
+	printf("!!! MMDevApi || RefreshWasapiDeviceList Not Implemented !!!\n");
 }
 
 void RestoreBitstreamOut_X()
 {
-
+	printf("!!! MMDevApi || RestoreBitstreamOut Not Implemented !!!\n");
 }
 
 void SetWasapiThreadAffinityMask_X()
 {
-
+	printf("!!! MMDevApi || SetWasapiThreadAffinityMask Not Implemented !!!\n");
 }
